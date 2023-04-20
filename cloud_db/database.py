6648e39c89a36c9b570ed0016735e7cb101cc40b
mathbrahian <mathbrahian@gmail.com>
2023-04-20 16:17:07 -0500

    se añadio port en la configuracion

--- cloud_db/database.py
+++ cloud_db/database.py
@@ -5,13 +5,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def url_postgresql(username, host, database, password):
+def url_postgresql(username, host, database, password, port):
     url = URL.create(
         drivername="postgresql",
         username=username,
         host=host,
         database=database,
         password=password,
+        port=port
     )
     return url
 
@@ -20,5 +21,6 @@ engine = create_engine(url_postgresql(
         host=os.getenv("HOST"),
         database=os.getenv("DATABASE"),
         password=os.getenv("PASSWORD"),
+        port=os.getenv("PORT")
         )
     )
